25c78be6a5f3e84a6d67483ce342a0170a586505
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-08-29 12:21:08 +0530

    GenericClass Demo

--- DS_Day2/Program.cs
+++ DS_Day2/Program.cs
@@ -10,21 +10,34 @@ namespace DS_Day2
     {
         static void Main(string[] args)
         {
-            UserStack s = new UserStack();
-            Console.WriteLine("IEmpty= " + s.isStackEmpty());
-            Console.WriteLine("------------");
-            s.Push(10);
-            s.Push(100);
-            s.Push(20);
-            s.Push(40);
-            Console.WriteLine("Pop ");
-            s.Pop();
-            Console.WriteLine("---------------------");
-            Console.WriteLine("Peek");
-            s.Peek();
-            Console.WriteLine("---------------------");
-            s.PrintStackElements();
-            Console.WriteLine("IEmpty= " + s.isStackEmpty());
+            //UserStack s = new UserStack();
+            //Console.WriteLine("IEmpty= " + s.isStackEmpty());
+            //Console.WriteLine("------------");
+            //s.Push(10);
+            //s.Push(100);
+            //s.Push(20);
+            //s.Push(40);
+
+            //Console.WriteLine("Pop ");
+            //s.Pop();
+            //Console.WriteLine("---------------------");
+            //Console.WriteLine("Peek");
+            //s.Peek();
+            //Console.WriteLine("---------------------");
+            //s.PrintStackElements();
+            //Console.WriteLine("IEmpty= " + s.isStackEmpty());
+
+
+            UserDataStore<string> stringdata = new UserDataStore<string>();
+            stringdata.AddOrEdit(0, "Hello");
+          string printdata  =stringdata.ReturnData(0);
+            Console.WriteLine(printdata);
+            Console.WriteLine("-------------------");
+            stringdata.AddOrEdit(0, "Welcome");
+            
+            stringdata.ShowData(0);
+
+
             Console.ReadLine();
         }
     }
