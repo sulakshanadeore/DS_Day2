7018097518d42cb0ceb3a108b5cae4bf2fc04f36
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-08-29 11:50:22 +0530

    stack demo

--- DS_Day2/Program.cs
+++ DS_Day2/Program.cs
@@ -10,6 +10,22 @@ namespace DS_Day2
     {
         static void Main(string[] args)
         {
+            UserStack s = new UserStack();
+            Console.WriteLine("IEmpty= " + s.isStackEmpty());
+            Console.WriteLine("------------");
+            s.Push(10);
+            s.Push(100);
+            s.Push(20);
+            s.Push(40);
+            Console.WriteLine("Pop ");
+            s.Pop();
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Peek");
+            s.Peek();
+            Console.WriteLine("---------------------");
+            s.PrintStackElements();
+            Console.WriteLine("IEmpty= " + s.isStackEmpty());
+            Console.ReadLine();
         }
     }
 }
---
+++ DS_Day2/UserStack.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Day2
+{
+    //LIFO
+    //Push,Pop,Peek
+    class UserStack
+    {
+    internal static readonly int max = 1000;
+internal int[] stack = new int[max];
+        int top;
+
+        internal bool isStackEmpty()
+        {
+            //bool status = false;
+            //if (top<0)
+            //{
+            //    status = true;
+            //}
+            //return status;
+            return (top < 0);
+        }
+
+        public UserStack()
+        {
+            top = -1;
+        }
+
+        internal void Push(int data)
+        {
+            if (top>=max)
+            {
+                Console.WriteLine("No space on the stack");
+                return;
+            }
+            else
+            {
+                stack[++top] = data;
+
+            }
+        }
+
+        internal void   Pop()
+        {
+            int value = 0;
+            if (top<0)
+            {
+                Console.WriteLine("No elements on the stack");
+                return;
+            }
+            else
+            {
+                value = stack[top--];
+                Console.WriteLine(value);
+            }
+        }
+     internal   void Peek()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("No elements on the stack");
+                return;
+            }
+            else
+            {
+                int value = stack[top];
+                Console.WriteLine(value);
+            }
+        }
+
+      internal  void PrintStackElements()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("No elements on the stack");
+                return;
+            }
+            else
+            {
+                for (int i = top; i >=0; i--)
+                {
+                    Console.WriteLine(stack[i]);
+                }
+            }
+        }
+    }
+}
